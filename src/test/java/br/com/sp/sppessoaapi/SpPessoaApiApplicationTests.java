a635d15aaf0214cca5d2568f91be9c1f88affcc7
Filipe Benevenuti <filipe.benevenuti@farmaciassaojoao.com.br>
2021-04-29 22:36:39 -0300

    Inicialização do projeto via spring initializr (https://start.spring.io/)

---
+++ src/test/java/br/com/sp/sppessoaapi/SpPessoaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sp.sppessoaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpPessoaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
